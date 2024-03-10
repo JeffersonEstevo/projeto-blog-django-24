8cf0c7ae4bd1f10b7970094091c33f1832913c89
JeffersonEstevo <jefferson.estevo@gmail.com>
2024-03-10 20:55:08 -0300

    Filtrando e ordenando os posts enviados para o template da view do Django

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -2,15 +2,18 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from blog.models import Post
 
-
-posts = list(range(1000))
-
+PER_PAGE = 9
 
 def index(request):
-    posts = Post.objects.order_by('-pk')
+    posts = (
+        Post
+        .objects
+        .filter(is_published=True)
+        .order_by('-pk')
+    )
 
 
-    paginator = Paginator(posts, 9)
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
@@ -24,9 +27,6 @@ def index(request):
 
 
 def page(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
 
     return render(
         request,
@@ -38,9 +38,6 @@ def page(request):
 
 
 def post(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
 
     return render(
         request,
